c21ac74453e132f15f83bc42f4a94a5785345b8b
mocklee <mock7ee@gmail.com>
2017-08-18 18:30:53 -0700

    added files pulled from device

---
+++ BoardConfig.mk
@@ -0,0 +1,32 @@
+USE_CAMERA_STUB := true
+
+# inherit from the proprietary version
+-include vendor/lge/lv517/BoardConfigVendor.mk
+
+TARGET_ARCH := arm
+TARGET_NO_BOOTLOADER := true
+TARGET_BOARD_PLATFORM := msm8937
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_VARIANT := cortex-a7
+TARGET_CPU_SMP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+TARGET_BOOTLOADER_BOARD_NAME := lv517
+
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 user_debug=30 msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 androidboot.hardware=lv517
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_PAGESIZE := 2048
+
+# fix this up by examining /proc/mtd on a running device
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x2000000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x2000000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0xE6666666
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x5F3333333
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+TARGET_PREBUILT_KERNEL := device/lge/lv517/prebuilt/kernel
+TARGET_KERNEL_CONFIG   := lv517_tmo_us-perf_defconfig
+
+BOARD_HAS_NO_SELECT_BUTTON := true
